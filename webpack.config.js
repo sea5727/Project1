1bfc76fe938362c9408d3d37579333467b7d19a0
ysh8361 <ysh8361@localhost.localdomain>
2019-07-27 05:22:56 +0900

    서버와 첫 웹페이즈 연동까지 구현

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+ 
+const outputDirectory = "dist";
+ 
+module.exports = {
+  entry: "./src/client/index.js",
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    port: 4000,
+    open: true,
+    disableHostCheck : true,
+    host: "0.0.0.0",
+    // proxy: {
+    //   "/api": "http://192.168.0.15:3000"
+    // }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
