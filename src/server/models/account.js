f825baca520c60e6094e576d871626bf20891f3c
ysh8361 <ysh8361@localhost.localdomain>
2019-08-04 18:13:14 +0900

    login and register END

--- src/server/models/account.js
+++ src/server/models/account.js
@@ -13,9 +13,6 @@ const Account = new Schema({
     }
 });
 
-exports.generateHash = (password) => {
-    return bcrypt.hashSync(password, 8)
-}
 
 exports.Account = mogoose.model('account', Account);
 
